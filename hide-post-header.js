54d81b80dc9e059d6ce22deb0a508553b35e2dc2
Lowell Kissling <lrkissling@gmail.com>
2017-02-25 23:52:41 -0800

    simplified by removing PageAction and activeTab stuff

--- hide-post-header.js
+++ hide-post-header.js
@@ -1,49 +1,18 @@
 const css_hidden = ".post-header.fixed {visibility: hidden}";
-const css_visible = ".post-header.fixed {visibility: visible}";
-const TITLE_HIDDEN = "Hidden";
-const TITLE_VISIBLE = "Visible";
 const target = "*://imgur.com/a/*"
 
-function toggleCSS(tab) {
-
-  function gotTitle(title) {
-      if (title === TITLE_HIDDEN) {
-        browser.pageAction.setIcon({tabId: tab.id, path: "icons/on.svg"});
-        browser.pageAction.setTitle({tabId: tab.id, title: TITLE_VISIBLE});
-        browser.tabs.removeCSS({code: css_visible});
-        browser.tabs.insertCSS({code: css_hidden});
-      } else {
-        browser.pageAction.setIcon({tabId: tab.id, path: "icons/off.svg"});
-        browser.pageAction.setTitle({tabId: tab.id, title: TITLE_HIDDEN});
-        browser.tabs.removeCSS({code: css_hidden});
-        browser.tabs.insertCSS({code: css_visible});
-      }
-  }
-
-  var gettingTitle = browser.pageAction.getTitle({tabId: tab.id});
-  gettingTitle.then(gotTitle);
-}
-
-
 /*
-Initialize the page action: set icon and title, then show.
+Inserts css to the appropriate tab to hide the post header.
 */
-function initializePageAction(responseDetails) {
-    browser.pageAction.setIcon({tabId: responseDetails.tabId, path: "icons/off.svg"});
-    browser.pageAction.setTitle({tabId: responseDetails.tabId, title: TITLE_HIDDEN});
-    browser.pageAction.show(responseDetails.tabId);
+function hidePostHeader(responseDetails) {
+    browser.tabs.insertCSS(responseDetails.tabId, {code: css_hidden});
 }
 
 /*
-Calls initializePageAction upon completion of page load for
+Calls hidePostHeader upon completion of page load for
 imgur.com/a/* urls.
 */
 browser.webRequest.onCompleted.addListener(
-  initializePageAction,
+  hidePostHeader,
   {urls: [target]}
 );
-
-/*
-Toggle CSS when the page action is clicked.
-*/
-browser.pageAction.onClicked.addListener(toggleCSS);
